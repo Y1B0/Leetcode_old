4d646b203cbab82d53dcfc2060ba881d3c37f804
Y1B0 <Y1B0@users.noreply.github.com>
2017-09-26 21:08:19 -0400

    Add to List 17. Letter Combinations of a Phone Number

--- 17. Letter Combinations of a Phone Number.py
+++ 17. Letter Combinations of a Phone Number.py
@@ -1,7 +1,20 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
-        d = {"1":"","2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
+def letterCombinations(digits):
+    """
+    :type digits: str
+    :rtype: List[str]
+    """
+    d = {"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
+    res = []
+    if len(digits) == 0:
+        return [""]
+    old = letterCombinations(digits[:-1]) # combination except the last one
+    lastdigit = digits[-1]
+    letters = d.get(lastdigit)
+    for letter in letters:
+        for string in old:
+            res.append(string + letter)
+#            print letter
+#    print res
+    return res
+
+print letterCombinations("234")

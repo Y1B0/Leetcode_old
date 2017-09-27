d0b15ca83adf67e14e73cf544847f83d9a342eb4
Y1B0 <Y1B0@users.noreply.github.com>
2017-09-26 22:12:17 -0400

    20. Valid Parentheses

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for c in s:
+            if c in ["{","[","("]：
+            stack.push(c)

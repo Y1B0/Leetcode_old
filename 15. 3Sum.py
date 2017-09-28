033e427db09b9f2ef7db415221d005aa42d94230
Y1B0 <Y1B0@users.noreply.github.com>
2017-09-28 00:01:26 -0400

    15. 3Sum
    还是比较standard的解法

---
+++ 15. 3Sum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            l = i+1
+            r = len(nums)-1
+            while l < r:
+                s = nums[i]+nums[l]+nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    comb = [nums[i],nums[l],nums[r]]
+                    print comb
+                    if not comb in res:
+                        res.append(comb)
+                        
+                    l += 1
+                    r -= 1
+                    
+        return res
+
+s = Solution()
+print s.threeSum([-1,0,1,2,-1,-4])
